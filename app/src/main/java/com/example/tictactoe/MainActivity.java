0a520333162fe2b3c547efdb9ffb7f9b268a5c6b
Leo <leota246@gmail.com>
2019-06-25 16:36:11 -0400

    commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    static Button button_00, button_01, button_02, button_10, button_11, button_12, button_20, button_21, button_22;
+    static TextView score1, score2;
+    static Button newgame;
+    static int turn, s1, s2;
+    static String p1 = "X";
+    static String p2 = "O";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button_00 = (Button) findViewById(R.id.button_00);
+        clicked(button_00);
+
+        button_01 = (Button) findViewById(R.id.button_01);
+        clicked(button_01);
+
+        button_02 = (Button) findViewById(R.id.button_02);
+        clicked(button_02);
+
+        button_10 = (Button) findViewById(R.id.button_10);
+        clicked(button_10);
+
+        button_11 = (Button) findViewById(R.id.button_11);
+        clicked(button_11);
+
+        button_12 = (Button) findViewById(R.id.button_12);
+        clicked(button_12);
+
+        button_20 = (Button) findViewById(R.id.button_20);
+        clicked(button_20);
+
+        button_21 = (Button) findViewById(R.id.button_21);
+        clicked(button_21);
+
+        button_22 = (Button) findViewById(R.id.button_22);
+        clicked(button_22);
+
+        newgame = (Button) findViewById(R.id.newgame);
+        newgame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clear();
+            }
+        });
+
+        score1 = (TextView) findViewById(R.id.score1);
+        score2 = (TextView) findViewById(R.id.score2);
+    }
+
+    public void clicked(final Button button) {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (turn % 2 == 0) {
+                    button.setText(p1);
+                } else {
+                    button.setText(p2);
+                }
+                if (button_00.getText().equals(button_01.getText()) && button_01.getText().equals(button_02.getText())) {
+                    whoWon(button_00);
+                }if (button_00.getText().equals(button_10.getText()) && button_10.getText().equals(button_20.getText())) {
+                    whoWon(button_00);
+                }if (button_10.getText().equals(button_11.getText()) && button_11.getText().equals(button_12.getText())) {
+                    whoWon(button_10);
+                }if (button_20.getText().equals(button_21.getText()) && button_21.getText().equals(button_22.getText())) {
+                    whoWon(button_20);
+                }if (button_01.getText().equals(button_11.getText()) && button_11.getText().equals(button_21.getText())) {
+                    whoWon(button_01);
+                }if (button_02.getText().equals(button_12.getText()) && button_12.getText().equals(button_22.getText())) {
+                    whoWon(button_02);
+                }if (button_00.getText().equals(button_11.getText()) && button_11.getText().equals(button_22.getText())) {
+                    whoWon(button_00);
+                }if (button_02.getText().equals(button_11.getText()) && button_11.getText().equals(button_20.getText())) {
+                    whoWon(button_02);
+                }
+                button.setClickable(false);
+                turn++;
+            }
+        });
+    }
+
+    public static void clear() {
+        button_00.setText("");
+        button_00.setClickable(true);
+        button_01.setText("");
+        button_01.setClickable(true);
+        button_02.setText("");
+        button_02.setClickable(true);
+        button_10.setText("");
+        button_10.setClickable(true);
+        button_11.setText("");
+        button_11.setClickable(true);
+        button_12.setText("");
+        button_12.setClickable(true);
+        button_20.setText("");
+        button_20.setClickable(true);
+        button_21.setText("");
+        button_21.setClickable(true);
+        button_22.setText("");
+        button_22.setClickable(true);
+    }
+
+
+    public void whoWon(Button button) {
+        if (button.getText().equals(p1)) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Congratulation!")
+                    .setMessage("Player 1 Won")
+                    .setPositiveButton("New Game", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            clear();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+            score1.setText(Integer.toString(++s1));
+        } else if (button.getText().equals(p2)) {
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Congratulation!")
+                    .setMessage("Player 2 Won")
+                    .setPositiveButton("New Game", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            clear();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+            score2.setText(Integer.toString(++s2));
+        }
+    }
+}
